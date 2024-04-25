5ded3789927d2132a4e84341d5c86a161da99e65
Ivan Chen <144068027+duhrice@users.noreply.github.com>
2024-04-24 22:47:21 -0400

    Modified sumAll function to add all non-negative integers between two specified numbers. Enabled all test cases in spec.js to ensure all test cases pass.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || num1 < 0 || !Number.isInteger(num2) || num2 < 0) {
+        return 'ERROR';
+    } else {
+        // Sum of Integers formula: S = n(a + l)/2
+        // S = total sum
+        // n = # of integers
+        // a = first term
+        // l = last term
+        return ((Math.abs(num1 - num2) + 1) * (num1 + num2)) / 2;
+    }
 };
 
 // Do not edit below this line
