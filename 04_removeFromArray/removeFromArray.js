4bc52eb24be077af958b2fc13fa6cab038cc148f
Ivan Chen <144068027+duhrice@users.noreply.github.com>
2024-04-24 22:25:52 -0400

    Modified removeFromArray function to remove specified elemets from a given array. Enabled all tests in spec.js to ensure all test cases are passed.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const inputArray = array;
 
+    for (const arg of args) {
+        let repeatCount = 0;
+
+        for (const item of inputArray) {
+            if (item == arg) {
+                repeatCount++;
+            }
+        }
+
+        for (let i = 0; i < repeatCount; i++) {
+            if (inputArray.indexOf(arg) == -1) {
+                continue;
+            } else {
+                inputArray.splice(inputArray.indexOf(arg), 1)
+            }
+        }
+    }
+
+    return inputArray;
 };
 
 // Do not edit below this line
