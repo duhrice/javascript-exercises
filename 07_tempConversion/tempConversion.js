48b420b6031ac85d9b9cbe78958582cd42e09d3f
Ivan Chen <144068027+duhrice@users.noreply.github.com>
2024-04-24 23:03:14 -0400

    Modified convertToCelsius and convertToFahrenheit function to convert a given temperature to a different measurement. Enabled all test cases to ensure all test cases pass.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let convertedTemp = (temperature - 32) * (5 / 9);
+
+  if (Number.isInteger(convertedTemp)) {
+    return convertedTemp
+  } else {
+    return parseFloat(convertedTemp.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let convertedTemp = (temperature * 9 / 5) + 32;
+  
+  if (Number.isInteger(convertedTemp)) {
+    return convertedTemp
+  } else {
+    return parseFloat(convertedTemp.toFixed(1));
+  }
 };
 
 // Do not edit below this line
